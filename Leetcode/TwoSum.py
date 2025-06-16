63e9602c86833e85d980b95edeca55b272ccef21
Camilovelez1 <camilo95022@gmail.com>
2025-06-16 07:26:34 -0500

    feat: add LongestCommonPrefix

--- Leetcode/TwoSum.py
+++ Leetcode/TwoSum.py
@@ -1,15 +1,20 @@
 nums = [2,7,11,15]
 target = 9
 
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-         listr=[]
-         i=0
-        for x in nums:
-            y = x - target
-            if y == x[i+1]:
-                x1=x[i]
-                x2=x[i+1]
-                listr.apend(x1)
-                listr.apend(x2)
-            else: i += 1
-        return listr
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complement_map = {}
+    
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            # If the complement is found in the map, return the solution
+            if complement in complement_map:
+                return [complement_map[complement], i]
+            
+            # Store the current number's index as a potential complement for future numbers
+            complement_map[num] = i
+            
+        # Return an empty list if no solution is found
+        # This line is optional depending on if you want to signal no solution explicitly
+        return []
